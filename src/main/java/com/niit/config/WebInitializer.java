90c7eab10171d44cc480bb7c07a5ff0a745e9699
ShubhamRDurugkar <35094667+ShubhamRDurugkar@users.noreply.github.com>
2018-04-10 12:24:34 +0530

    Chat implementation done.

--- src/main/java/com/niit/config/WebInitializer.java
+++ src/main/java/com/niit/config/WebInitializer.java
@@ -1,18 +1,42 @@
 package com.niit.config;
 
+import java.nio.charset.StandardCharsets;
+
+import javax.servlet.Filter;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected void customizeRegistration(ServletRegistration.Dynamic registration) {
+		System.out.println("customizeRegistration");
+		registration.setInitParameter("dispatchOptionRequest", "true");
+		registration.setAsyncSupported(true);
+	}
+
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
-	return new Class[] { WebResolver.class , DBConfig.class};
+		return new Class[] { WebResolver.class, DBConfig.class };
 	}
+
 	@Override
 	protected Class<?>[] getServletConfigClasses() {
-		return null;}
+		return null;
+	}
 
 	@Override
 	protected String[] getServletMappings() {
-	return new String[] { "/" };
+		return new String[] { "/" };
+	}
+
+	@Override
+	protected Filter[] getServletFilters()
+	{
+		CharacterEncodingFilter encodingFilter=new CharacterEncodingFilter();
+		encodingFilter.setEncoding(StandardCharsets.UTF_8.name());
+		return new Filter[] {encodingFilter};
 	}
 }
--- src/main/java/com/niit/controller/ProfilePictureController.java
+++ src/main/java/com/niit/controller/ProfilePictureController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
-import com.niit.daoimpl.ProfileUpdateDAO;
+import com.niit.dao.ProfileUpdateDAO;
 import com.niit.model.ProfilePicture;
 import com.niit.model.UserDetail;
 
@@ -22,34 +22,7 @@ public class ProfilePictureController {
 	@Autowired
 	ProfileUpdateDAO profileUpdateDAO;
 	
-	/*@RequestMapping(value="/uploadProfilePicture",method=RequestMethod.POST)
-	public ResponseEntity<?> uploadProfilePicture(@RequestParam(value="image")  CommonsMultipartFile image,HttpSession session){
-		try{
-			UserDetail userDetail=(UserDetail)session.getAttribute("loginname");
-			if(userDetail==null){
-	    		return new ResponseEntity<String>("Unauthorized access",HttpStatus.UNAUTHORIZED);
-	    	}
-			ProfilePicture profilePicture=new ProfilePicture();
-			profilePicture.setLoginname(userDetail.getLoginname());
-			profilePicture.setImage(image.getBytes());
-			profilePictureDAO.insertOrUpdateProfilePicture(profilePicture);
-			return new ResponseEntity<ProfilePicture>(profilePicture,HttpStatus.OK);
-    	}
-		catch(Exception e){
-    		System.out.print(e);
-    		return new ResponseEntity<String>("Internal server error",HttpStatus.INTERNAL_SERVER_ERROR);
-    	}
-	}
-
-	@RequestMapping(value="/getProfilePicture/{loginname}",method=RequestMethod.GET)
-	public @ResponseBody byte[] getProfilePicture(@PathVariable String loginname){
-			ProfilePicture profilePicture=profilePictureDAO.getProfilePicture(loginname);
-			if(profilePicture==null)
-				return null;
-			return profilePicture.getImage();
-    	
-	}*/
-	@RequestMapping(value="/doUpload",method=RequestMethod.POST)
+		@RequestMapping(value="/doUpload",method=RequestMethod.POST)
 	public ResponseEntity<?> uploadPicture(@RequestParam(value="file")CommonsMultipartFile fileupload,HttpSession session)
 	{
 	
@@ -72,7 +45,7 @@ public class ProfilePictureController {
 	}
 	
 	@RequestMapping(value="/getImage/{loginname}")
-	public @ResponseBody byte[] getProfilePic(@PathVariable("loginame") String loginname)
+	public @ResponseBody byte[] getProfilePic(@PathVariable("loginname") String loginname)
 	{
 		
 		ProfilePicture profilePicture=profileUpdateDAO.getProfilePicture(loginname);
